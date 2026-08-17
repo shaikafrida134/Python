019df90b5c2e69af7b91d06ef1cbfd5a204c2ca1
shaikafrida134 <shaikafrida134@gmail.com>
2026-08-17 22:51:12 +0530

    Create variable.py

---
+++ variable.py
@@ -0,0 +1,12 @@
+A_Ameesha = "Afrida"
+B_Afrida = "Ameesha"
+_cAreefa = "Venky"
+Dkushida_ = "Nagur"
+__EBasha2 = "Beebi"
+
+
+print(A_Ameesha)
+print(B_Afrida)
+print(_cAreefa)
+print(Dkushida_)
+print(__EBasha2)
